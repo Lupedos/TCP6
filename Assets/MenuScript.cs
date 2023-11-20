f75e58cf25af8e9474a2b3f74136aa5c3d629ec1
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2023-11-20 18:54:34 -0300

    update: menu Ui arte e canvas config

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -5,27 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-   
-  public void Jogar()
-  {
-    SceneManager.LoadScene(4);
-  } 
-  public void Creditos()
-  {
-    SceneManager.LoadScene(3);
-  }
-  public void Info()
-  {
-    SceneManager.LoadScene(2);
-  }
-  public void Niveis()
-  {
-    SceneManager.LoadScene(1);
-  }
 
-  public void Inicio()
-  {
-    SceneManager.LoadScene(0);
-  }
-  
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Creditos()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Info()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Niveis()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+
+    public void Config()
+    {
+
+    }
+
+    public void Inicio()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
